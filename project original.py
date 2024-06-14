b41d87b572e52ed14f9d498b82c10f0860ffbd22
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-06-14 15:27:26 +0900

    Update project original.py

--- project original.py
+++ project original.py
@@ -15,45 +15,8 @@ insert_director_sql = f"""insert into {director_table} (name) values(%s);"""
 insert_movie_director_sql = f"""insert into {movie_director_table} (movie_id, director_id) values(%s,%s);"""
 select_director_id_sql = f"""select id from {director_table} where name=%s;"""
 
-
-def process_movie_row(row, cur):
-    title = row[0]
-    eng_title = row[1]
-    year = row[2]
-    country = row[3]
-    m_type = row[4]
-    genre = row[5]
-    status = row[6]
-    directors = row[7]
-    company = row[8]
-
-    try:
-        cur.execute(insert_movie_sql, (title, eng_title, year, country, m_type, status, company))
-        movie_id = cur.lastrowid
-
-        cur.execute(insert_genre_sql, (movie_id, genre))
-
-        if directors is None:
-            return
-
-        director_ids = []
-        for director in directors.split(","):
-            cur.execute(select_director_id_sql, director.strip())
-            director_id = cur.fetchone()
-            if director_id:
-                director_ids.append(director_id['id'])
-            else:
-                cur.execute(insert_director_sql, director.strip())
-                director_id = cur.lastrowid
-                director_ids.append(director_id)
-
-        for director_id in director_ids:
-            cur.execute(insert_movie_director_sql, (movie_id, director_id))
-
-    except Exception as e:
-        pass
-        print(e)
-        print(row)
+select_last_movie_id_sql = f"""select max(id) as id from {movie_table};"""
+select_last_director_id_sql = f"""select max(id) as id from {director_table};"""
 
 
 def read_excel_into_mysql():
@@ -110,19 +73,77 @@ def read_excel_into_mysql():
 
     df1 = df1.replace({np.nan: None})
     df2 = df2.replace({np.nan: None})
+    dfs = [df1, df2]
 
-    for i, r in df1.iterrows():
-        process_movie_row(tuple(r), cur)
-        if (i + 1) % 1000 == 0:
-            print(f"{i} rows")
+    movie_rows = []
+    director_rows = []
+    genre_rows = []
+    for df in dfs:
+        for movie_id, r in df.iterrows():
+            row = tuple(r)
+            title = row[0]
+            eng_title = row[1]
+            year = row[2]
+            country = row[3]
+            m_type = row[4]
+            genre = row[5]
+            status = row[6]
+            directors = row[7]
+            company = row[8]
 
-    for i, r in df2.iterrows():
-        process_movie_row(tuple(r), cur)
-        if (i + 1) % 1000 == 0:
-            print(f"{i} rows")
+            movie_rows.append(
+                (title, eng_title, year, country, m_type, status, company))
+            director_rows.append(directors)
+            genre_rows.append(genre)
+
+    cur.executemany(insert_movie_sql, movie_rows)
+    print("inserted movies")
+
+    cur.execute(select_last_movie_id_sql)
+    last_movie_id = cur.fetchone()["id"]
+    first_movie_id = last_movie_id - len(movie_rows) + 1
+
+    for movie_id in range(first_movie_id, last_movie_id + 1):
+        row_index = movie_id - first_movie_id
+        genre_rows[row_index] = (movie_id, genre_rows[row_index])
+
+    genre_rows = [x for x in genre_rows if x is not None]
+    cur.executemany(insert_genre_sql, genre_rows)
+    print("inserted genres")
+
+    unique_directors = set()
+    for directors in director_rows:
+        if directors is None:
+            continue
+        for director in directors.split(","):
+            unique_directors.add(director.strip())
+
+    unique_directors = list(unique_directors)
+    cur.executemany(insert_director_sql, unique_directors)
+    conn.commit()
+    print("inserted directors")
+
+    cur.execute(select_last_director_id_sql)
+    last_director_id = cur.fetchone()["id"]
+    first_director_id = last_director_id - len(unique_directors) + 1
+
+    director_id_map = {}
+    for director_id in range(first_director_id, last_director_id + 1):
+        row_index = director_id - first_director_id
+        director_id_map[unique_directors[row_index]] = director_id
+
+    movie_director_rows = []
+    for movie_id in range(first_movie_id, last_movie_id + 1):
+        row_index = movie_id - first_movie_id
+        if director_rows[row_index] is None:
+            continue
+        for director in director_rows[row_index].split(","):
+            movie_director_rows.append(
+                (movie_id, director_id_map[director.strip()]))
+    cur.executemany(insert_movie_director_sql, movie_director_rows)
+    print("inserted movie_directors")
 
     conn.commit()
-
     close_db(conn, cur)
 
 
